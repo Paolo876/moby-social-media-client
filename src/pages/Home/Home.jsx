447f5f8fc9f80d1ae65895e4da91eb8a4755dba8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-09 00:25:45 -0800

    updated home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
-import useAuthRedux from '../../hooks/useAuthRedux'
+import { useEffect } from 'react';
+import useAuthRedux from '../../hooks/useAuthRedux';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const { user } = useAuthRedux();
+  const navigate = useNavigate();
 
+  useEffect(()=>{
+    if(!user) navigate("/login")
+  },[ user ])
   return (
     <div>Home</div>
   )
