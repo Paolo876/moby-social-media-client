f8021e39d7ad32184ce7b4ca5c86aa44d38b94b7
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-15 00:16:48 -0800

    created UserStatusDropdown component

--- src/pages/Home/UserNavigation.jsx
+++ src/pages/Home/UserNavigation.jsx
@@ -3,6 +3,7 @@ import { Grid, Paper, ButtonBase, Typography, List, ListItem, ListItemButton, Li
 import Image from '../../components/Image';
 import useAuthRedux from '../../hooks/useAuthRedux';
 import defaultAvatar from "../../assets/default-profile.png"
+import UserStatusDropDown from './UserStatusDropDown';
 const UserNavigation = () => {
   const { user } = useAuthRedux();
   let image;
@@ -11,25 +12,30 @@ const UserNavigation = () => {
     <Paper variant="outlined" sx={{m:.5, p: 1, height: "100%"}}>
         <List>
             <ListItem disablePadding>
-                <ListItemButton sx={{pl: 3}}>
-                <ListItemIcon sx={{border: 3, borderColor: "green", minWidth: 0, borderRadius: 50, mr: 3}}>
-                    {image ? 
-                        <Image 
-                            src={image.url} 
-                            transformation={[{
-                                height: 40,
-                                width: 40,
-                            }]} 
-                            style={{borderRadius: "50%"}}
-                            /> :
-                        <img src={defaultAvatar} style={{height: "40px", width: "40px"}}/>
-                    }
-                </ListItemIcon>
+                <ListItemButton sx={{pl: 3}} onClick={() => console.log("/profile")}>
+                    <ListItemIcon sx={{border: 3, borderColor: "green", minWidth: 0, borderRadius: 50, mr: 3}}>
+                        {image ? 
+                            <Image 
+                                src={image.url} 
+                                transformation={[{
+                                    height: 40,
+                                    width: 40,
+                                }]} 
+                                style={{borderRadius: "50%"}}
+                                /> :
+                            <img src={defaultAvatar} style={{height: "40px", width: "40px"}}/>
+                        }
+                    </ListItemIcon>
                     <Stack>
                         <Typography variant="body1">@{user.username}</Typography>
                         <Typography variant="body2">{user.UserData.firstName} {user.UserData.lastName}</Typography>
                     </Stack>
                 </ListItemButton>
+            </ListItem>
+            <ListItem>
+            <ListItemButton>
+                <UserStatusDropDown/>
+            </ListItemButton>
             </ListItem>
         </List>
     </Paper>
