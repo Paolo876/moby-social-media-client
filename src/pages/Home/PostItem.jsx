9c641fc13ca037315d3c30cf595945a3d4d4302b
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-21 23:46:55 -0800

    updated postItem ui

--- src/pages/Home/PostItem.jsx
+++ src/pages/Home/PostItem.jsx
@@ -10,64 +10,75 @@ import TurnedInIcon from '@mui/icons-material/TurnedIn';
 import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
 import usePostsRedux from '../../hooks/usePostsRedux';
 
-const PostBody = ({ isPublic, title, user, image: coverImage, isHovered, transitions, postText }) => {
+const PostBody = ({ isPublic, title, user, image: coverImage, isHovered, transitions, postText, isBookmarked }) => {
   let image;
   if(coverImage) image = JSON.parse(coverImage);
   return (
-  <Button 
-    sx={{
-      width: "100%", mr: "auto", p:0, 
-      textTransform: "none", textAlign: "left", 
-      borderRadius: "10px 10px 0 0", 
-      transition: transitions.create('filter', {duration: 1200, delay: 0})
-      }} 
-      color="primary"
-    >
-    <Box 
-      sx={{
-        position: "relative", 
-        width: "100%", p: 0, color: "black", minHeight: {xs: 170, sm:200}, maxHeight: {xs: 220, sm:300, md: 330, lg: 350}, borderRadius: "10px 10px 0 0", overflow: "hidden", backgroundColor: "white"
-      }} 
-      variant="outlined"
-      >
-      {!isPublic && <Tooltip title="Private posts are only visible to friends of the author." arrow >
-        <LockIcon fontSize="small" sx={{position: "absolute", top: 10, left: 10, opacity: .9, zIndex: 5}} color="info"/>
-      </Tooltip>}
-      <Stack  
+    <>
+      <Tooltip title={isBookmarked ? "You bookmarked this post." : "Bookmark Post"} arrow leaveDelay={50}>
+        <IconButton sx={{ borderRadius: 1, position: "absolute", zIndex: 20, right: 0}} >
+          {isBookmarked ? 
+            <TurnedInIcon fontSize="medium" sx={{color: "rgba(239, 144, 60, 1)"}}/> : 
+            <TurnedInNotIcon fontSize="medium" sx={{color: "rgba(239, 144, 60, 1)"}}/>
+          }
+        </IconButton>
+      </Tooltip>
+      <Button 
         sx={{
-          position:"absolute", top: "45%", left: "50%", transform: "translate(-50%,-50%)", 
-          zIndex: 2, width: "100%", 
-          backgroundColor: isHovered ? "rgba(225,225,225,0.5)" : "", 
-          py: 2,
-          transition: transitions.create('all', {duration: 800, delay: 0}),
-          textShadow: image ? "1px 1px 5px rgba(255,255,255,0.25)" : "initial",
-          }}
+          width: "100%", mr: "auto", p:0, 
+          textTransform: "none", textAlign: "left", 
+          borderRadius: "10px 10px 0 0", 
+          transition: transitions.create('filter', {duration: 1200, delay: 0})
+          }} 
+          color="primary"
         >
-        <Typography variant="h5" align='center' fontWeight={600} pb={isHovered ? .75 :.5} color="primary.dark">{title}</Typography>
-        {isHovered && isPublic && <Typography variant="subtitle1" align='center' sx={{width: "100%", opacity: .9}} mb={2} px={2}>{postText.length > 150 ? postText.substr(0,150) : postText}...</Typography>}
-        {isHovered && !isPublic && <Typography variant="subtitle1" align='center' sx={{width: "100%", opacity: .9}} mb={2} px={2} fontStyle={isPublic ? "" : "italic"}>This post is private.</Typography>}
-        <Typography variant="body2" align={isHovered ? 'right' : 'center'} fontWeight={400} sx={{opacity: .6}} px={4}>-{user.UserDatum.firstName} {user.UserDatum.lastName}</Typography>
-      </Stack>
-      {image && 
-        <Image 
-          src={image.url}
-          alt={image.name}
-          style={{
-            objectFit: "cover", 
-            objectPosition: "center",
-            width: "100%", 
-            height: "100%", 
-            transform: "translate(0, 0)", 
-            zIndex: 1, 
-            transition: transitions.create('filter', {duration: 800, delay: 0}), filter: isPublic && isHovered ? "" : "blur(1px) opacity(0.5) grayscale(.5) brightness(0.65)"
-            }}
-          />}
-    </Box>
-  </Button>
+        <Box 
+          sx={{
+            position: "relative", 
+            width: "100%", p: 0, color: "black", minHeight: {xs: 170, sm:200}, maxHeight: {xs: 220, sm:300, md: 330, lg: 350}, borderRadius: "10px 10px 0 0", overflow: "hidden", backgroundColor: "white"
+          }} 
+          variant="outlined"
+          >
+          {!isPublic && <Tooltip title="Private posts are only visible to friends of the author." arrow >
+            <LockIcon fontSize="small" sx={{position: "absolute", top: 10, left: 10, opacity: .9, zIndex: 5}} color="info"/>
+          </Tooltip>}
+          <Stack  
+            sx={{
+              position:"absolute", top: "48%", left: "50%", transform: "translate(-50%,-50%)", 
+              zIndex: 2, width: "100%", 
+              backgroundColor: isHovered ? "rgba(225,225,225,0.5)" : "", 
+              py: 2,
+              transition: transitions.create('all', {duration: 800, delay: 0}),
+              textShadow: image ? "1px 1px 5px rgba(255,255,255,0.25)" : "initial",
+              }}
+            >
+            <Typography variant="h5" align='center' fontWeight={600} pb={isHovered ? .75 :.5} color="primary.dark">{title}</Typography>
+            {isHovered && isPublic && <Typography variant="subtitle1" align='center' sx={{width: "100%", opacity: .9}} mb={2} px={2}>{postText.length > 150 ? postText.substr(0,150) : postText}...</Typography>}
+            {isHovered && !isPublic && <Typography variant="subtitle1" align='center' sx={{width: "100%", opacity: .9}} mb={2} px={2} fontStyle={isPublic ? "" : "italic"}>This post is private.</Typography>}
+            <Typography variant="body2" align={isHovered ? 'right' : 'center'} fontWeight={400} sx={{opacity: .6}} px={4}>-{user.UserDatum.firstName} {user.UserDatum.lastName}</Typography>
+          </Stack>
+          {image && 
+            <Image 
+              src={image.url}
+              alt={image.name}
+              style={{
+                objectFit: "cover", 
+                objectPosition: "center",
+                width: "100%", 
+                height: "100%", 
+                transform: "translate(0, 0)", 
+                zIndex: 1, 
+                transition: transitions.create('filter', {duration: 800, delay: 0}), filter: isPublic && isHovered ? "" : "blur(1px) opacity(0.5) grayscale(.5) brightness(0.65)"
+                }}
+              />}
+        </Box>
+      </Button>
+    </>
+  
   )
 }
 
-const PostActions = ({ palette, isLiked, isBookmarked, user, userImage, createdAt, id}) => {
+const PostActions = ({ palette, isLiked, isBookmarked, user, userImage, createdAt, id, likes}) => {
   const navigate = useNavigate();
   const { likePost, isLoading } = usePostsRedux();
   return (
@@ -80,16 +91,7 @@ const PostActions = ({ palette, isLiked, isBookmarked, user, userImage, createdA
                 <FavoriteIcon fontSize="medium" sx={{color: "rgba(229, 85, 85, 1)"}}/> : 
                 <FavoriteBorderIcon fontSize="medium" sx={{color: "rgba(229, 85, 85, .85)"}}/>
               }
-              <Typography variant="body2" color="rgba(0, 0, 0, .45)" sx={{ml:.5}}>Like</Typography>
-            </IconButton>
-          </Tooltip>
-          <Tooltip title={isBookmarked ? "You bookmarked this post." : "Bookmark Post"} arrow leaveDelay={50}>
-            <IconButton sx={{py:.75, px: 2, mr:.25, borderRadius: 5}}>
-              {isBookmarked ? 
-                <TurnedInIcon fontSize="medium" sx={{color: "rgba(239, 144, 60, 1)"}}/> : 
-                <TurnedInNotIcon fontSize="medium" sx={{color: "rgba(239, 144, 60, 1)"}}/>
-              }
-              <Typography variant="body2" color="rgba(0, 0, 0, .45)" sx={{ml:.5}}>Bookmark</Typography>
+              <Typography variant="body2" color="rgba(255, 255, 255, .75)" sx={{ml:1}}>{likes > 0 && likes}</Typography>
             </IconButton>
           </Tooltip>
         </Stack>
@@ -114,7 +116,7 @@ const PostActions = ({ palette, isLiked, isBookmarked, user, userImage, createdA
   )
 }
 
-const PostItem = ({ title, image, isPublic, postText, isLiked=false, isBookmarked=false, user, createdAt, updatedAt, id}) => {
+const PostItem = ({ title, image, isPublic, postText, isLiked=false, isBookmarked=false, user, createdAt, updatedAt, id, likes}) => {
   const [ isHovered, setIsHovered ] = useState(false)
   const { palette, transitions } = useTheme();
   let userImage;
@@ -122,13 +124,13 @@ const PostItem = ({ title, image, isPublic, postText, isLiked=false, isBookmarke
   return (
     <Grid 
       item 
-      sx={{m:.5, mt: 2, p: 0, boxShadow: 3, mb: 3, borderRadius: "10px", transition: transitions.create('all', {duration: 800, delay: 0}), "&:hover": { boxShadow: 5 }}} 
+      sx={{m:.5, mt: 2, p: 0, boxShadow: 3, mb: 3, borderRadius: "10px", position: "relative", transition: transitions.create('all', {duration: 800, delay: 0}), "&:hover": { boxShadow: 5 }}} 
       xs={12}
       onMouseOver={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       >
       <PostBody isPublic={isPublic} createdAt={createdAt} title={title} user={user} image={image} isHovered={isHovered} transitions={transitions} postText={postText}/>
-      <PostActions palette={palette} isLiked={isLiked} isBookmarked={isBookmarked} user={user} userImage={userImage} createdAt={createdAt} id={id}/>
+      <PostActions palette={palette} isLiked={isLiked} isBookmarked={isBookmarked} user={user} userImage={userImage} createdAt={createdAt} id={id} likes={likes}/>
     </Grid>
   )
 }
