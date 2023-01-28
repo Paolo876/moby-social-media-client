418b229f1fa0f0b85272944198fa0cfe5aaf8038
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-27 22:47:39 -0800

    updated profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
 import AuthorizedPageContainer from '../../components/AuthorizedPageContainer'
 const Profile = () => {
+  const UserId = useParams()["*"];
+  const [ user, setUser ] = useState(null);
+  useEffect(() => {
+    if(UserId){
+      //fetch user data from UserId
+    } else {
+
+    }
+  }, [])
+
   return (
     <AuthorizedPageContainer>
         <div>Profile</div>
