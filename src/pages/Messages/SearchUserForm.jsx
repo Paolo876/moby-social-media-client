e13fda48377075df83e7e6e359e6b40020d84688
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-30 22:34:59 -0800

    updated search user form on message component

--- src/pages/Messages/SearchUserForm.jsx
+++ src/pages/Messages/SearchUserForm.jsx
@@ -1,14 +1,63 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import useAuthRedux from '../../hooks/useAuthRedux';
-import { Typography, Stack, Tooltip, Button, Box } from '@mui/material';
+import { Typography, Stack, Tooltip, Button, Box, Modal, TextField, List, ListItemButton, ListItem } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import axios from 'axios';
+import LoadingSpinner from "../../components/LoadingSpinner"
+import defaultAvatar from "../../assets/default-profile.png"
+import Image from '../../components/Image';
+
+
+const style = {
+  position: 'absolute',
+  top: '40%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  minWidth: 300,
+  borderRadius: 1,
+  bgcolor: 'background.paper',
+  boxShadow: 10,
+  p: 2,
+};
 
 
 const SearchUserForm = () => {
   const { user } = useAuthRedux();
   const [ showModal, setShowModal ] = useState(false);
+  const [ input, setInput ] = useState("")
+  const [ inputResponse, setInputResponse ] = useState("");
+  const [ users, setUsers ] = useState(null);
+  const [ isLoading, setIsLoading ] = useState(false)
 
+  useEffect(() => {
+    let timeout;
+    if(input.trim().length === 0) {
+      setUsers(null)
+      setIsLoading(false)
+    };
+    if(input.trim().length !== 0 && inputResponse.toLowerCase() !== input.toLowerCase()){
+      setIsLoading(true)
+      timeout = setTimeout(() => {
+        setInputResponse(input)
+        axios.get(`${process.env.REACT_APP_DOMAIN_URL}/api/auth/search/${input}`, { headers: { 'Content-Type': 'application/json' }, withCredentials: true })
+        .then(res => {
+          console.log(res.data)
+          setUsers(res.data)
+          setIsLoading(false)
+        })
+      }, 500);
+    
+    }
+    return () => clearTimeout(timeout)
+  }, [input]);
 
+  const handleCloseModal = () => {
+    setShowModal(false)
+    setInput("")
+    setInputResponse("")
+    setUsers(null)
+    setIsLoading(false)
+  }
   return (
     <Stack flexDirection="row" alignItems="center" justifyContent="center" mx={2} my={1} >
         <Box backgroundColor="rgba(0,0,0,0.1)" borderRadius={4}  sx={{flex:1, height: "100%", py:.25, mx: .5}} >
@@ -17,6 +66,52 @@ const SearchUserForm = () => {
         <Tooltip title="Write a new message" arrow>
             <Button color="secondary" size="small" variant="contained" sx={{minWidth: 0, ml: 1}} onClick={() => setShowModal(true)}><EditIcon fontSize='small'/></Button>
         </Tooltip>
+
+        {/* modal */}
+        <Modal
+          open={showModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+          onClose={handleCloseModal}
+        >
+          <Box sx={style}>
+            <Typography variant="h6" align="left" mb={2}>Send a New Message</Typography>
+            <TextField 
+              id="standard-basic" 
+              label="Enter Receipient's Name" 
+              variant="standard" 
+              fullWidth 
+              sx={{mb:2}}
+              value={input}  
+              onChange={e => setInput(e.target.value)}
+              />
+            <List>
+              <ListItem alignItems="flex-start" sx={{justifyContent: "center", p:0, position: "relative", minHeight: "1.5em"}}>
+                {isLoading && <LoadingSpinner style={{minHeight: "0em", backgroundColor: "initial", transform: "scale(.5)", opacity: .75, position: "absolute", left: "35%", top: "-100%"}}/>}
+              </ListItem>
+              {users && users.map(item => 
+                <ListItemButton sx={{ }} key={item.id} >
+                  {item.UserDatum.image ? 
+                    <Image 
+                      src={JSON.parse(item.UserDatum.image).url} 
+                      transformation={[{
+                          height: 35,
+                          width: 35,
+                      }]} 
+                      style={{borderRadius: "50%"}}
+                      alt="profile-avatar"
+                    /> :
+                    <img src={defaultAvatar} style={{height: "35px", width: "35px"}} alt="profile-avatar"/>
+                  }
+                  <Stack ml={1}>
+                    <Typography variant="body1" align='left'>{item.username}</Typography>
+                    <Typography variant="body2" align='left'>{item.UserDatum.firstName} {item.UserDatum.lastName}</Typography>
+                  </Stack>
+                </ListItemButton>
+              )}
+            </List>
+          </Box>
+        </Modal>
     </Stack>
   )
 }
