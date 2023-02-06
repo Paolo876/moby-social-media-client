0f4ae3a54df7bfe6093172a3267a2e1770f476b8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-06 00:50:34 -0800

    updated settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import AuthorizedPageContainer from '../../components/AuthorizedPageContainer'
-import { Container, Divider, Paper, Typography } from '@mui/material'
-import SettingsIcon from '@mui/icons-material/Settings';
+import { Container, Divider, Paper, Typography, Grid } from '@mui/material'
 import useProfileActions from '../../hooks/useProfileActions';
 import useAuthRedux from '../../hooks/useAuthRedux';
 import MyTextField from '../../components/MyTextField';
@@ -16,7 +15,8 @@ import * as Yup from 'yup';
 const validationSchema = Yup.object().shape({
   firstName: Yup.string().min(1).max(15).required(),
   lastName: Yup.string().min(1).max(20).required(),
-  birthday: Yup.date()
+  birthday: Yup.date(),
+  userBioBody : Yup.string().min(1).max(255),
 })
 
 
@@ -26,16 +26,17 @@ const Settings = () => {
   const [ isImageNew, setIsImageNew ] = useState(false)
   const [ image, setImage ] = useState(JSON.parse(UserData.image) ? JSON.parse(UserData.image).url : null)
   const [ showDate, setShowDate ] = useState(false);
+  const [ initialValues, setInitialValues ] = useState(null)
+  
 
-  const initialValues = {
-    firstName: UserData.firstName,
-    lastName: UserData.lastName,
-    birthday: UserData.birthday,
-  }
-
-  // useEffect(() => {
-  //   getProfileById().then(data => console.log(data))
-  // }, [])
+  useEffect(() => {
+    getProfileById().then(data => setInitialValues({
+      firstName: data.UserDatum.firstName,
+      lastName: data.UserDatum.lastName,
+      birthday: data.UserDatum.birthday,
+      userBioBody : data.UserBio.body,
+    }))
+  }, [])
 
   const handleImageChange = (data) => {
     setImage(data)
@@ -49,62 +50,79 @@ const Settings = () => {
   return (
     <AuthorizedPageContainer>
       <Container sx={{pt: 1.5}}>
-      <Paper sx={{py: 5, px: 2, width: "fit-content", mx: "auto" }} elevation={4}>
-        <Typography variant="h4" fontWeight={700} mb={4} letterSpacing={1}><SettingsIcon style={{verticalAlign:"middle"}} sx={{mr: 1}} fontSize="large"/>Settings</Typography>
-        <Divider/>
-        <Formik  
-            initialValues={initialValues}
-            onSubmit={handleSubmit} 
-            validationSchema={validationSchema}
-          >
-          <Form style={{display: "flex", flexDirection: "column", my: 5}}>
-            <UploadImageForm
-              setImage={handleImageChange} 
-              isImageNew={isImageNew}
-              image={image} 
-              title="Update Profile Picture"
-              defaultImage={defaultAvatar}
-              previewStyle={{ height: "100px", width: "100px", borderRadius: "50%" }}
-              width={200}
-              height={200}
-              border={20}
-              borderRadius={100}
-            />
-            <MyTextField 
-              id="firstName" 
-              name="firstName"
-              type="text" 
-              label={<p>First Name</p>} 
-              variant="standard" 
-              sx={{my:1, minWidth: "320px"}}
-              error="First Name is Required."
-            />
-            <MyTextField 
-              id="lastName" 
-              name="lastName"
-              type="text" 
-              label={<p>Last Name</p>} 
-              variant="standard" 
-              sx={{my:1, minWidth: "320px"}}
-              error="First Name is Required."
-            />
-              <MyTextField 
-                id="birthday" 
-                name="birthday"
-                type={showDate ? "date" : "text" }
-                onFocus={() => setShowDate(true)}
-                onBlur={(e) => {
-                  if(e.target.value === "") setShowDate(false)
-                }}
-                label={<p>Birthday</p>} 
-                variant="standard" 
-                sx={{my:1, minWidth: "320px"}}
-                error="Last Name is Required."
-              />
-          </Form>
-        </Formik>
-
-      </Paper>
+        <Grid container direction="row" alignItems="flex-start" sx={{justifyContent: {xs: "center"}, height: "75vh"}}>
+        {initialValues && 
+          <Grid item xs={12} md={8} py={2}>
+              <Paper sx={{py: 5, px: {xs: 2, md:8}, width: "100%", mx: "auto" }} elevation={4}>
+                <Typography variant="h4" fontWeight={700} mb={2} letterSpacing={1}>Settings</Typography>
+                <Formik  
+                    initialValues={initialValues}
+                    onSubmit={handleSubmit} 
+                    validationSchema={validationSchema}
+                  >
+                  <Form style={{display: "flex", flexDirection: "column", my: 5}}>
+                    <Typography variant="h5" mt={2} fontWeight={500}>User Data</Typography>
+                    <Divider/>
+                    <UploadImageForm
+                      setImage={handleImageChange} 
+                      isImageNew={isImageNew}
+                      image={image} 
+                      title="Update Profile Picture"
+                      defaultImage={defaultAvatar}
+                      previewStyle={{ height: "100px", width: "100px", borderRadius: "50%" }}
+                      width={200}
+                      height={200}
+                      border={20}
+                      borderRadius={100}
+                    />
+                    <MyTextField 
+                      id="firstName" 
+                      name="firstName"
+                      type="text" 
+                      label={<p>First Name</p>} 
+                      variant="standard" 
+                      sx={{my:1, minWidth: "320px"}}
+                      error="First Name is Required."
+                    />
+                    <MyTextField 
+                      id="lastName" 
+                      name="lastName"
+                      type="text" 
+                      label={<p>Last Name</p>} 
+                      variant="standard" 
+                      sx={{my:1, minWidth: "320px"}}
+                      error="First Name is Required."
+                    />
+                    <MyTextField 
+                      id="birthday" 
+                      name="birthday"
+                      type={showDate ? "date" : "text" }
+                      onFocus={() => setShowDate(true)}
+                      onBlur={(e) => {
+                        if(e.target.value === "") setShowDate(false)
+                      }}
+                      label={<p>Birthday</p>} 
+                      variant="standard" 
+                      sx={{my:1, minWidth: "320px"}}
+                      error="Last Name is Required."
+                    />
+                    <Typography variant="h5" mt={3} fontWeight={500}>User Bio</Typography>
+                    <Divider/>
+                    <MyTextField 
+                      id="userBioBody" 
+                      name="userBioBody"
+                      type="text" 
+                      label="User Bio"
+                      variant="outlined" 
+                      rows={4}
+                      multiline
+                      sx={{my:1, width: "100%"}}
+                    />
+                  </Form>
+                </Formik>
+            </Paper>
+          </Grid>}
+        </Grid>
       </Container>
     </AuthorizedPageContainer>
   )
