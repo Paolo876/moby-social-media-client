9995e32a9192afc40114e3973a614c0432b2e11f
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-16 20:37:21 -0800

    updated search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,7 +1,8 @@
 import { Button, Paper, Typography } from '@mui/material';
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import AuthorizedPageContainer from '../../components/AuthorizedPageContainer'
+import axios from 'axios';
 
 import { Container, Alert, Grid } from "@mui/material"
 
@@ -10,14 +11,30 @@ const Search = () => {
   const [ searchParams, setSearchParams ] = useSearchParams()
   const query = searchParams.get("q");
 
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
+  const [ result, setResult ] = useState(null);
+
 
   useEffect(() => {
-    if(query) {
-      console.log(query)
-    }
+    if(query) fetchData();
   }, [query])
 
+  const fetchData = async () => {
+    setIsLoading(true)
+    try {
+        const res = await axios.get(`${process.env.REACT_APP_DOMAIN_URL}/api/main/search?${searchParams.toString()}`, { headers: { 'Content-Type': 'application/json' }, withCredentials: true });
+        setIsLoading(false)
+        setResult(res)
+    } catch(err) {
+      setIsLoading(false)
+      setError((err.response && err.response.data) ? err.response.data.message : err.message)
+    }
+  }
 
+
+  console.log(result)
+  
   return (
     <AuthorizedPageContainer>
       <Container>
