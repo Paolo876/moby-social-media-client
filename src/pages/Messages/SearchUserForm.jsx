707841289de132eb6dd718eabe401f0e3f3e03d1
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-17 22:43:34 -0800

    updated searchuserform component

--- src/pages/Messages/SearchUserForm.jsx
+++ src/pages/Messages/SearchUserForm.jsx
@@ -3,11 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import useAuthRedux from '../../hooks/useAuthRedux';
 import useChatRedux from '../../hooks/useChatRedux';
 import useMessagesActions from '../../hooks/useMessagesActions';
-import { Typography, Stack, Tooltip, Button, Box, Modal, TextField, List, ListItemButton, ListItem, Fade, Alert } from '@mui/material';
+import { Typography, Stack, Tooltip, Button, Box, Modal, TextField, List, ListItemButton, ListItem, Fade, Alert, Divider, IconButton, ButtonBase } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import LoadingSpinner from "../../components/LoadingSpinner"
 import defaultAvatar from "../../assets/default-profile.png"
 import Image from '../../components/Image';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 
 const style = {
@@ -70,13 +71,27 @@ const SearchUserForm = () => {
 
   
   return (
-    <Stack flexDirection="row" alignItems="center" justifyContent="center" mx={2} my={2} >
-        <Box backgroundColor="rgba(0,0,0,0.1)" borderRadius={4}  sx={{flex:1, height: "100%", py:.25, mx: .5}} >
+    <Stack flexDirection="row" alignItems="center" justifyContent="center">
+        <Box backgroundColor="rgba(0,0,0,0.1)" borderRadius={4}  sx={{flex:1, height: "100%", mx: 2, my: 1}} >
             <Typography variant="h6" fontSize={16} align="center">{user.username}</Typography>
         </Box>
         <Tooltip title="Write a new message" arrow>
-            <Button color="secondary" size="small" variant="contained" sx={{minWidth: 0, ml: 1}} onClick={() => setShowModal(true)}><EditIcon fontSize='small'/></Button>
+            <Button color="secondary" size="small" variant="contained" sx={{minWidth: 0, mr: 1}} onClick={() => setShowModal(true)}><EditIcon fontSize='small'/></Button>
         </Tooltip>
+        <Divider orientation="vertical" flexItem sx={{ml:.5}}/>
+        <IconButton
+          // aria-controls={open ? 'split-button-menu' : undefined}
+          // aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          // onClick={handleToggle}
+          sx={{borderRadius: "0"}}
+          color="info"
+          variant="outlined"
+          edge={false}
+        >
+          <ArrowDropDownIcon sx={{height: "100%"}}/>
+        </IconButton>
 
         {/* modal */}
         <Modal
