31dae24ef2386ef787982479ee4ccd4be7e35afe
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-28 22:49:59 -0800

    updated snackbar component

--- src/components/SnackbarComponent.jsx
+++ src/components/SnackbarComponent.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { SnackbarContent } from 'notistack'
+import { Box, Paper, Stack, Typography, ButtonBase } from '@mui/material'
 
 const SnackbarComponent = React.forwardRef((props, ref) => {
-    const {
-      // You have access to notistack props and options 👇🏼
-      id,
-      content,
-      ...other
-    } = props
+    const { content: { title = null, image, header, subheader, id, type, link}, ...other } = props
   
-    console.log(content)
+    console.log(title)
     return (
       <SnackbarContent ref={ref} {...other}>
+        <ButtonBase  sx={{width:"100%"}}>
+          <Paper sx={{width:"100%", opacity: .8}} elevation={2}>
+            <Stack>
+              <Box>
+                {/* image here */}
+              </Box>
+              <Stack>
+                {title && <Typography>{title}</Typography>}
+                <Typography>{header}</Typography>
+                <Typography>{subheader}</Typography>
+              </Stack>
+            </Stack>
+          </Paper>
+        </ButtonBase>
       </SnackbarContent>
     )
   })
@@ -19,3 +29,4 @@ const SnackbarComponent = React.forwardRef((props, ref) => {
 SnackbarComponent.displayName = "SnackbarComponent"
 
 export default SnackbarComponent
+
