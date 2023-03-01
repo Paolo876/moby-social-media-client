c5d9399601aab094902697e390e10b9f18e5a1c0
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-28 23:01:53 -0800

    updated snackbar component

--- src/components/SnackbarComponent.jsx
+++ src/components/SnackbarComponent.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { SnackbarContent } from 'notistack'
 import { Box, Paper, Stack, Typography, ButtonBase } from '@mui/material'
+import defaultAvatar from "../assets/default-profile.png";
 
 const SnackbarComponent = React.forwardRef((props, ref) => {
     const { content: { title = null, image, header, subheader, id, type, link}, ...other } = props
-  
-    console.log(title)
+    const navigate = useNavigate();
+
     return (
       <SnackbarContent ref={ref} {...other}>
-        <ButtonBase  sx={{width:"100%"}}>
-          <Paper sx={{width:"100%", opacity: .8}} elevation={2}>
+        <ButtonBase  sx={{width:"100%"}} onClick={() => navigate(link)}>
+          <Paper sx={{width:"100%", background: "rgba(100,100,100, .95)"}} elevation={3}>
             <Stack>
               <Box>
                 {/* image here */}
               </Box>
-              <Stack>
-                {title && <Typography>{title}</Typography>}
-                <Typography>{header}</Typography>
-                <Typography>{subheader}</Typography>
+              <Stack alignItems="left" justifyContent="left">
+                {title && <Typography align="left">{title}</Typography>}
+                <Typography align="left" color="white" variant="body2">{header}</Typography>
+                <Typography align="left" color="white" variant="body1">{subheader}</Typography>
               </Stack>
             </Stack>
           </Paper>
