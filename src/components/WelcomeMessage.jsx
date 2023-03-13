29cab882bd9d778dc747da2b4ef94190d9ddd2a8
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-12 22:12:13 -0700

    updated welcome message component

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -9,15 +9,15 @@ const WelcomeMessage = () => {
     <Paper sx={{p:2, background: "none", display: {xs: "none", md: "initial"}, maxWidth: "40%"}} elevation={0}>
         <Typography variant="h4" mb={3} sx={{alignSelf: "flex-start"}}>Welcome to Moby!</Typography>
         <Typography variant="subtitle1" mb={2} sx={{alignSelf: "flex-start"}} lineHeight={1.45}>
-            Moby is a social media concept web app that has the essential features of a modern social media web application. 
-            <br/>This includes the implementation of CRUD operations and a realtime database management system (chat system, friend status updates, notifications) using socket.io.
-            <br/>Private and sensitive data such as passwords and messages are encrypted using bcryptjs to ensure data privacy and security.
+            Moby is a social media web app that incorporates the essential features of a modern social media application. 
+            <br/>This includes the implementation of CRUD operations and a realtime database management system (chat system, friend updates, notification system) using socket.io.
+            <br/>Data encryption is implemented on passwords using <strong>bcryptjs</strong> to ensure privacy and security.
         </Typography>
         <Typography variant="subtitle2" mb={8} sx={{alignSelf: "flex-start"}}>
-            Created using ReactJS, ReduxJSToolkit, ExpressJS, NodeJS, MySQL, Socket.io, and other related libraries.
+            This app is created using ReactJS, ReduxJSToolkit, ExpressJS, NodeJS, MySQL, Socket.io, and other related libraries.
         </Typography>
         <Typography variant="subtitle2" sx={{alignSelf: "flex-start"}}>
-            Designed and developed by Paolo Bugarin.
+            Designed and developed by <strong>Paolo Bugarin</strong>.
         </Typography>
         <Box>
           <Tooltip title="Facebook" arrow><IconButton size="medium" color="secondary" target="_blank" href="https://www.facebook.com/paolobugarin19/"><FacebookIcon fontSize="inherit"/></IconButton></Tooltip>
