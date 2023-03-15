6473885be2a3ac6f18fd969593eec55548862fe0
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-15 00:51:46 -0700

    updated notifications page ui

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import useNotificationRedux from "../../hooks/useNotificationRedux"
+import AuthorizedPageContainer from '../../components/AuthorizedPageContainer'
+import { Alert, Container, Grid, Box, Paper, Typography, Divider, Button, ButtonBase, Stack } from '@mui/material'
 
 const Notifications = () => {
+  const { notifications, isLoading, error } = useNotificationRedux();
+
   return (
-    <div>Notifications</div>
+    <AuthorizedPageContainer>
+      <Container>
+        {error && <Box my={2}><Alert severity='error'>{error}</Alert></Box>}
+          <Grid container>
+            <Paper sx={{my: 2, py: 5, px: {xs: 2, md:8}, mx: "auto", height: "fit-content", position: "relative"}} elevation={4}>
+            </Paper>
+          </Grid>
+      </Container>
+    </AuthorizedPageContainer>
   )
 }
 
